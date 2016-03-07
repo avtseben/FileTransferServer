a511cec186c6f85a813269813e9c60c555847035
Alexander Tsebenko <sas@mbh.mbhtech>
2016-03-07 21:53:31 +0600

    Add_String_TRansfer

--- src/CatchFile.java
+++ src/CatchFile.java
@@ -2,13 +2,24 @@ import java.io.*;
 
 public class CatchFile {
 
-    private InputStream inStream; //inputstream it just bytes
-    private FileOutputStream fos;
-
+private InputStream inStream; //inputstream it just bytes
+private FileOutputStream fos;
+private boolean byteTransferMode = false;
+private BufferedReader in;
+private PrintWriter out;
+private String file;
+    
     CatchFile (InputStream _inStream) {
+	file = "file";
 	inStream = _inStream;
 	try {
-	    fos = new FileOutputStream("file");
+	    in = new BufferedReader(
+        	new InputStreamReader(inStream));
+	    out = new PrintWriter(
+		new BufferedWriter(
+	    new FileWriter(file)), true);//AutoFlush
+
+    	    fos = new FileOutputStream(file);
 	} catch (IOException e) {
 	}
 
@@ -17,11 +28,18 @@ public class CatchFile {
    public void writeByChars() {
     try {
 	char c;
+	String inLine = new String();
 	while(true) {
+	    if(byteTransferMode) {
 	    c = (char)inStream.read();
 	    fos.write(c);
 	    if(c == 'Q') break; //TODO get EOF 
+	    } else {
+		if((inLine = in.readLine()) == null) break;
+		out.println(inLine);
+	    }
 	}
+	out.close();
 	fos.close();
     } catch (IOException e) {}
    }
